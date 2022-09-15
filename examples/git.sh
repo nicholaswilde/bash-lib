32df6a80e5789f8a85ed9d0017ce4f59f7f36001
nιcнolaѕ wιlde <ncwilde43@gmail.com>
2022-09-15 12:14:16 -0700

    feat: update
    Signed-off-by: nιcнolaѕ wιlde <ncwilde43@gmail.com>

--- examples/git.sh
+++ examples/git.sh
@@ -4,9 +4,30 @@
 source ../init
 source "${LIBBASH_DIR}/git"
 
+function check_git(){
+  if lb_git_available; then
+    printf "git is available\n"
+  else
+    printf "git is not available\n"
+  fi
+}
+
+function check_in_repo(){
+  if lb_in_git_repo; then
+    # shellcheck disable=SC2016
+    printf '$(pwd) is in a repo\n'
+  else
+    # shellcheck disable=SC2016
+    printf '$(pwd) is not in a repo\n'
+  fi
+}
+
 function main() {
-  printf "lib: git\n"
-  printf "repo_root: %s\n" "$(lb_repo_root)"
+  check_git
+  check_in_repo
+  
+  s=$(lb_repo_root)
+  printf "repo_root: %s\n" "${s}"
 }
 
 main "${@}"
